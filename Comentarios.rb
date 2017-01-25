a7821398134f19e8c5c325999d5ea43b47fb04df
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2017-01-25 12:03:44 -0600

    Crear comentarios en ruby

---
+++ Comentarios.rb
@@ -0,0 +1,21 @@
+=begin
+
+todo esto
+es
+un
+bloque
+de
+comentarios
+
+=end
+class HolaMundo #se define la clase
+  def initialize()
+  end
+  def saluda()
+    puts "Hola Amigos"
+  end
+end
+# usando el gato se crea un comentario a esa linea o fila
+objeto=HolaMundo.new()
+objeto.saluda
+gets()
